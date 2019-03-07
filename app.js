0ac891528f1da2b1b712296df27483d66ecd2c50
crushedsummers <hairshinethesalonpony@gmail.com>
2019-03-07 21:24:02 +0800

    base design 0.1

--- app.js
+++ app.js
@@ -1,85 +1,124 @@
-var currentlocation = [];
-var currentdesc = [];
-var descno = 5;
-var locationno = 0;
-var ingame = document.getElementById("in-game");
+/*MAIN MENU*/
+var icon = document.createElement("img");
+icon.src = "assets/pre.jpg";
+$(icon).css("border-radius", "20%");
+$(icon).attr("id", "icon");
 
-/* stats */
-var bg = "grey"
-var time = 16;
+$(icon).on("mouseover", function(){
+  $(icon).css("border-radius", "30%");
+})
 
-/*subdesc*/
-for(i = 0; i < 5; i++){ 
-	var subDesc = document.createElement("h4");
-  subDesc.classList.add("desc");
-  subDesc.style.color = "#d3d3d3";
-  subDesc.style.fontSize = "20px";
-  subDesc.style.backgroundColor = "grey";
-  ingame.appendChild(subDesc);
-} 
- 
-currentlocation[0] = "ABANDONED ROOM"; {
-  currentdesc[0] = " ";
-  currentdesc[1] = " ";
-  currentdesc[2] = " ";
-  currentdesc[3] = " ";
-  currentdesc[4] = " ";
-  currentdesc[5] = "..."; 
-  currentdesc[6] = "You wake up in a room greeted with grey, it appears as if you are in an abandoned room";
-  currentdesc[7] = "You stand up and adjust your eyes to feel your surroundings, and notice numerous items scattered across the floor aside from the spot you were lying on mere moments ago.";
-  currentdesc[8] = "However you are unable to examine your area easily due to the lack of light.";
-  currentdesc[9] = "It is getting dark soon";
-  currentdesc[10] = "You notice 2 doors and a window, one door facing in front of you, another to your left, and the window on your right.";
-  currentdesc[11] = "There is something on your wrist, It is a watch. It says: " + time + " ...It seems like your clothes are all scattered too.";
-  currentdesc[12] = "...";
-  currentdesc[13] = "You see a bubblewrap piece";
-  currentdesc[14] = "Will you pop it?";
-  currentdesc[15] = "...";
-  currentdesc[16] = "...";  
-  currentdesc[17] = "Dammit you didn't fall for the bait, Thanks for playing!";
-}
+$(icon).on("mouseout", function(){
+  $(icon).css("border-radius", "20%");
+})
 
-  
+$(icon).on("mousedown", function(){
+  $(this).css("box-shadow", "0px 0px");
+  $(this).css("animation-play-state", "paused");
+  $(this).css("transform", "scale(0.9)");
+})
 
-//start
- 
-ingame.style.backgroundColor = bg //current bg color
+$(icon).on("mouseup", function(){
+  $(this).css("box-shadow", "10px 10px");
+  $(this).css("transform", "scale(1)");
+  $(this).fadeOut("slow").queue(function() {
+  /* Checklist: 
+  √ button of bitlife icon at the center
+  √ "Start Life" at the bottom
+  */
 
-ingame.onclick = function() {
-  var location = document.getElementById("location");
-  var desc = document.getElementsByClassName("desc");
-  
-  location.innerHTML= currentlocation[locationno];
- 
-  for(i = 0; i < 5; i++){ 
-	  desc[i].innerHTML = currentdesc[descno - i];
-  }
-  
-  if(descno == 13) {
-    var popSound = document.createElement("audio");
-    popSound.src = "http://bit.ly/AX202_PopSound";
+    /*icons*/
+    var portraitPhases = ["assets/baby.png", "assets/child.png", "assets/girl.png"];
+
+    /*containers*/
+    var divId = ["sideMenu", "mainText", "stats"];
+
+    for (var i=0; i<2; i++){
+      var box = document.createElement("span");
+      $(box).attr("id", (divId[i]));
+      $(box).attr("class", "playy");
+      $("#inGame").append(box);
+    }
+
+    /*MAIN PAGE*/
+
+
+    /* Checklist:
+    - description
+    - prompts may often come up
+    - go through it in 6 months!!!!!! only aft elementary thou cuz i rmb it more
+    */
+
+    /*SIDE MENU*/
+    const name = "Raphaele Guillemot";
+    var currentAge = 0;
+    var currentPortrait = 0;
+    var status = ["Infant", "Student"];
+    var statusSrc = ["assets/babyIcon", "assets/studentIcon"];
+
+    //portrait
+    var portrait = document.createElement("img");
+    portrait.src = portraitPhases[currentPortrait];
+    $(portrait).attr("id", "portrait");
+    $(portrait).attr("class", "sideElements");
+    $("#sideMenu").append(portrait);
+    //name
+    var text = document.createElement("h2");
+    $(text).attr("id", "name");
+    $(text).attr("class", "sideElements");
+    $(text).html(name);
+    $("#sideMenu").append(text);
+    //ethnicity
+    var flag = document.createElement("img");
+    flag.src = "assets/flag.png";
+    $(flag).attr("id", "ethnicity");
+    $(flag).attr("class", "sideElements");
+    $("#sideMenu").append(flag);
+    //age up!
+    var levelUp = document.createElement("img");
+    levelUp.src = "assets/age+.png";
+    $(levelUp).attr("id", "levelUp");
+    $(levelUp).attr("class", "sideElements");
+    $(levelUp).click(function() {
+      if (currentAge != 16) {
+        currentAge += 1;
+        $("#location").html(currentAge+" Years Old");
+        console.log(currentAge);
+      }
+      else {
+        console.log("!!! reached end!");
+        //THE END SEQUENCE
+      }
+    });
+    $("#sideMenu").append(levelUp);
+
     
-    var newBubble = document.createElement("img");
-	  newBubble.src = "http://bit.ly/AX202_UnPop";
-	  newBubble.onclick = pop; 
+    /* Checklist:
+    √ name
+    √ ethnicity
+    - activities 
+      - indicates own choices/interests, list them up like the application!!
+    √ ability to level up one year!
+      √ stop at 16
+      > create end sequence!
+    */
 
-      function pop(){
-        if(event.target.src != "http://bit.ly/AX202_Pop"){
-          currentdesc[17] = "Nyahahhaha, you popped the bubble, Thanks for playing!";
-          popSound.play();
-	         }
-      
-	event.target.src = "http://bit.ly/AX202_Pop";
-}
+    /*STATS*/
+
+    /*Checklist:
+    - happiness
+    - health
+    - smarts
+    - looks (always low LOL)
+    */
+//   });
+// });
+
+// $("#inGame").append(icon);
+
+  // 
     
-document.getElementById("in-game").appendChild(newBubble);
-}
-  
-  if (descno != 17) {
-    descno += 1; 
-    
-      var clicc = document.createElement("audio");
-      clicc.src = "http://freesound.org/data/previews/399/399934_1676145-lq.mp3";
-      clicc.play();
-  }
-}
+  //     var clicc = document.createElement("audio");
+  //     $(clicc).src = "http://freesound.org/data/previews/399/399934_1676145-lq.mp3";
+  //     $(clicc).play();
+  // }
